51fb1140fd8cd334f7ae6919332c599f1b8e84c6
hchowdhury3273 <hc2324@nyu.edu>
2020-07-28 12:59:49 -0400

    added edit view page

--- Dream Journal/Dream Journal/ContentView.swift
+++ Dream Journal/Dream Journal/ContentView.swift
@@ -24,154 +24,6 @@ struct ContentView: View {
     }
 }
 
-struct JournalList: View {
-    @State var show = false
-    @State var txt = ""
-    @ObservedObject var data = getData()
-
-    var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                if !show {
-                     Text("Dream Journal")
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .opacity(0.7)
-                }
-                Spacer(minLength: 0)
-                HStack {
-                    if self.show {
-                        Image(systemName: Constants.searchIcon.rawValue)
-                            .padding(.horizontal, 8)
-                            .foregroundColor(.white)
-                        TextField("Search Dreams", text: self.$txt)
-                            .foregroundColor(.white)
-                        Button(action: {
-                            withAnimation {
-                                self.txt = ""
-                                self.show.toggle()
-                            }
-                        }) {
-                            Image(systemName: "xmark")
-                                .foregroundColor(.white
-                            )
-                        }
-                        .padding(.horizontal, 8)
-                    }
-                    else {
-                        Button(action: {
-                            withAnimation {
-                                self.show.toggle()
-                            }
-                        }) {
-                            Image(systemName: Constants.searchIcon.rawValue)
-                                .foregroundColor(.white).padding(10)
-                        }
-                    }
-                }
-                .padding(self.show ? 10 : 0)
-                .background(Color.blue)
-                .opacity(0.7)
-                .cornerRadius(20)
-            }
-            .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)! + 15)
-            .padding(.horizontal)
-            .padding(.bottom, 20)
-            
-            if self.data.datas.isEmpty {
-                if self.data.noData {
-                    Spacer()
-                    Text("There are no journals")
-                    Spacer()
-                }
-                else {
-                    Spacer()
-                    Indicator()
-                    Spacer()
-                }
-            }
-            else {
-                ScrollView(.vertical, showsIndicators: false) {
-                    if self.txt != "" {
-                        VStack(spacing: 15) {
-                            if self.data.datas.filter({$0.title.lowercased().contains(self.txt.lowercased())}).count == 0 {
-                                Text("No Results Found")
-                                    .padding(.top, 10)
-                            }
-                            else {
-                                ForEach(self.data.datas.filter({$0.title.lowercased().contains(self.txt.lowercased())})) {entry in
-                                    cellView(journal: entry)
-                                }
-                            }
-                        }
-                        .padding(.horizontal, 15)
-                        .padding(.top, 10)
-                        
-                    }
-                    else {
-                        VStack (spacing: 15) {
-                            ForEach(self.data.datas) {entry in
-                                cellView(journal: entry)
-                            }
-                        }
-                        .padding(.horizontal, 15)
-                        .padding(.top, 10)
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct cellView: View {
-    var journal: Journal
-
-    var body: some View {
-        ZStack {
-            Rectangle().fill(Color.white)
-                .cornerRadius(10)
-                .shadow(color: .gray, radius: 3, x: 1, y: 1)
-                .opacity(0.8)
-            VStack {
-                HStack {
-                    VStack (alignment: .leading) {
-                        Text(journal.title).bold()
-                            .padding(.top, 8.0)
-                        Text(journal.date)
-                            .font(.caption).padding(.bottom, 10.0)
-                        Text(journal.description)
-                            .font(.system(size: 14))
-                            .foregroundColor(.gray)
-                            .lineLimit(2)
-                            .padding(.bottom, 10.0)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
-                    .padding(.leading, 15.0)
-                    Spacer()
-                }
-//                HStack {
-//                    Spacer()
-//                    NavigationLink(destination: ViewDetails(deeplink: deeplink)) {
-//                        Text("View Details")
-//                            .foregroundColor(.blue)
-//                            .font(.system(size: 15))
-//                            .bold()
-//                    }
-//                    .padding(.trailing, 10)
-//                    NavigationLink(destination: ViewDetails(deeplink: deeplink)) {
-//                        Text("Edit")
-//                            .foregroundColor(.blue)
-//                            .font(.system(size: 15))
-//                            .bold()
-//                    }
-//                }
-//                .padding(.bottom, 8)
-//                .padding(.trailing, 15)
-            }
-        }.padding(.top, 1.0)
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -244,6 +96,62 @@ struct Indicator: UIViewRepresentable {
     }
     func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<Indicator>) {
     }
+}
+
+struct EditView: View {
+    @State var txt = ""
+    @Binding var show: Bool
     
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            MultiLineTF(txt: self.$txt)
+                .padding()
+                .background(Color.black.opacity(0.05))
+            Button(action: {
+                self.show.toggle()
+            }) {
+                Text("Save")
+                    .padding(.vertical)
+                    .padding(.horizontal, 25)
+                    .foregroundColor(.white)
+            }
+            .background(Color.blue.opacity(0.8))
+            .clipShape(Capsule())
+            .padding()
+        }
+    }
+}
+
+struct MultiLineTF: UIViewRepresentable {
+    func makeCoordinator() -> MultiLineTF.Coordinator {
+        return MultiLineTF.Coordinator(parent1: self)
+    }
     
+    @Binding var txt: String
+    
+    func makeUIView(context: UIViewRepresentableContext<MultiLineTF>) -> UITextView {
+        let view = UITextView()
+        view.text = "Type Something"
+        view.font = .systemFont(ofSize: 18)
+        view.textColor = .gray
+        view.isEditable = true
+        view.backgroundColor = .clear
+        view.delegate = context.coordinator
+        return view
+    }
+    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<MultiLineTF>) {
+    }
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: MultiLineTF
+        init(parent1: MultiLineTF) {
+            parent = parent1
+        }
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            textView.text = ""
+            textView.textColor = .black
+        }
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.txt = textView.text
+        }
+    }
 }
---
+++ Dream Journal/Dream Journal/Home.swift
@@ -0,0 +1,179 @@
+//
+//  Home.swift
+//  Dream Journal
+//
+//  Created by Helal Chowdhury on 7/28/20.
+//  Copyright © 2020 Helal. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct JournalList: View {
+    @State var show = false
+    @State var txt = ""
+    @State var show2 = false
+    @ObservedObject var data = getData()
+
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            VStack(spacing: 0) {
+                HStack {
+                    if !show {
+                         Text("Dream Journal")
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .foregroundColor(.blue)
+                            .opacity(0.7)
+                    }
+                    Spacer(minLength: 0)
+                    HStack {
+                        if self.show {
+                            Image(systemName: Constants.searchIcon.rawValue)
+                                .padding(.horizontal, 8)
+                                .foregroundColor(.white)
+                            TextField("Search Dreams", text: self.$txt)
+                                .foregroundColor(.white)
+                            Button(action: {
+                                withAnimation {
+                                    self.txt = ""
+                                    self.show.toggle()
+                                }
+                            }) {
+                                Image(systemName: "xmark")
+                                    .foregroundColor(.white
+                                )
+                            }
+                            .padding(.horizontal, 8)
+                        }
+                        else {
+                            Button(action: {
+                                withAnimation {
+                                    self.show.toggle()
+                                }
+                            }) {
+                                Image(systemName: Constants.searchIcon.rawValue)
+                                    .foregroundColor(.white).padding(10)
+                            }
+                        }
+                    }
+                    .padding(self.show ? 10 : 0)
+                    .background(Color.blue)
+                    .opacity(0.7)
+                    .cornerRadius(20)
+                }
+                .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)! + 15)
+                .padding(.horizontal)
+                .padding(.bottom, 20)
+                
+                if self.data.datas.isEmpty {
+                    if self.data.noData {
+                        Spacer()
+                        Text("There are no journals")
+                        Spacer()
+                    }
+                    else {
+                        Spacer()
+                        Indicator()
+                        Spacer()
+                    }
+                }
+                else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        if self.txt != "" {
+                            VStack(spacing: 15) {
+                                if self.data.datas.filter({$0.title.lowercased().contains(self.txt.lowercased())}).count == 0 {
+                                    Text("No Results Found")
+                                        .padding(.top, 10)
+                                }
+                                else {
+                                    ForEach(self.data.datas.filter({$0.title.lowercased().contains(self.txt.lowercased())})) {entry in
+                                        cellView(journal: entry)
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 15)
+                            .padding(.top, 10)
+                            
+                        }
+                        else {
+                            VStack (spacing: 15) {
+                                ForEach(self.data.datas) {entry in
+                                    cellView(journal: entry)
+                                }
+                            }
+                            .padding(.horizontal, 15)
+                            .padding(.top, 10)
+                        }
+                    }
+                }
+            }
+            
+            Button(action: {
+                self.show2.toggle()
+            }) {
+                Image(systemName: "plus")
+                    .resizable()
+                    .frame(width: 18, height: 18)
+                    .foregroundColor(.white)
+            }.padding()
+            .background(Color.blue)
+            .opacity(0.8)
+            .clipShape(Circle())
+            .padding()
+        }
+        .sheet(isPresented: self.$show2) {
+            EditView(show: self.$show2)
+        }
+    }
+}
+
+struct cellView: View {
+    var journal: Journal
+
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 3, x: 1, y: 1)
+                .opacity(0.8)
+            VStack {
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text(journal.title).bold()
+                            .padding(.top, 8.0)
+                        Text(journal.date)
+                            .font(.caption).padding(.bottom, 10.0)
+                        Text(journal.description)
+                            .font(.system(size: 14))
+                            .foregroundColor(.gray)
+                            .lineLimit(2)
+                            .padding(.bottom, 10.0)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    .padding(.leading, 15.0)
+                    Spacer()
+                }
+//                HStack {
+//                    Spacer()
+//                    NavigationLink(destination: ViewDetails(deeplink: deeplink)) {
+//                        Text("View Details")
+//                            .foregroundColor(.blue)
+//                            .font(.system(size: 15))
+//                            .bold()
+//                    }
+//                    .padding(.trailing, 10)
+//                    NavigationLink(destination: ViewDetails(deeplink: deeplink)) {
+//                        Text("Edit")
+//                            .foregroundColor(.blue)
+//                            .font(.system(size: 15))
+//                            .bold()
+//                    }
+//                }
+//                .padding(.bottom, 8)
+//                .padding(.trailing, 15)
+            }
+        }.padding(.top, 1.0)
+    }
+}
+
